adebced054ba84bcf885a9bbdfa7c99c905ae847
Juan Bernal Martinez <Juan Bernal Martinez@DESKTOP-Q8G3AKF>
2023-02-13 17:27:11 -0600

    Cambios con descuento

--- Impresoras/Class1.cs
+++ Impresoras/Class1.cs
@@ -14,24 +14,27 @@ namespace Impresoras
             
         }
         public abstract void aplicarDescuento(Venta venta, double iva);
-        public abstract void impresora(string impresora, int total);
+        //public abstract void impresora(string impresora, int total);
     }
     public class SocioFrecuente : estrategia
     {
         public SocioFrecuente()
         {
-      
+        
         }
         public override void aplicarDescuento(Venta venta, double iva)
         {
             Console.Write("Descuento por cliente frecuente");
+            Console.WriteLine("Venta de producto: " + venta.impresora + "Por un total de: " + (venta.total + ((venta.total/100)*iva)));
+            Console.WriteLine("Se aplicó un descuento del 50% por ser cliente frecuente");
+            Console.WriteLine("Total con descuento aplicado: " + (venta.total + ((venta.total / 100) * iva)/2));
         }
 
-        public override void impresora(string impresora, int total)
+        /*public void impresora(string impresora, int total)
         {
             venta.impresora = impresora;
             venta.total = total;
-        }
+        }*/
     }
 
     public class ClienteSinDescuento : estrategia
@@ -43,13 +46,14 @@ namespace Impresoras
         public override void aplicarDescuento(Venta venta, double iva)
         {
             Console.WriteLine("Cliente sin descuento");
+
         }
 
-        public override void impresora(string impresora, int total)
+        /*public override void impresora(string impresora, int total)
         {
             venta.impresora = impresora;
             venta.total = total;
-        }
+        }*/
     }
 
     public class Cliente5Por : estrategia
@@ -57,13 +61,17 @@ namespace Impresoras
         public override void aplicarDescuento(Venta venta, double iva)
         {
             Console.WriteLine("Descuento del 5 por ciento");
+            venta.total = venta.total + ((venta.total / 100) * 16);
+            Console.WriteLine("Venta de producto: " + venta.impresora + "Por un total de: " + (venta.total + ((venta.total / 100) * iva)));
+            Console.WriteLine("Se aplicó un descuento del 5%");
+            Console.WriteLine("Total con descuento aplicado: " + (venta.total - ((venta.total/100)*5)));
         }
 
-        public override void impresora(string impresora, int total)
+        /*public override void impresora(string impresora, int total)
         {
             venta.impresora = impresora;
             venta.total = total;
-        }
+        }*/
     }
 
     public class ClienteFijo : estrategia
@@ -73,11 +81,11 @@ namespace Impresoras
             Console.WriteLine("Descuento del $100 por su compra");
         }
 
-        public override void impresora(string impresora, int total)
+        /*public override void impresora(string impresora, int total)
         {
             venta.impresora = impresora;
             venta.total = total;
-        }
+        }*/
     }
 }
 
--- Impresoras/Program.cs
+++ Impresoras/Program.cs
@@ -13,15 +13,20 @@ namespace Impresoras
             estrategia Cliente2;
             estrategia Cliente3;
             estrategia Cliente4;
+            Venta venta1 = new Venta();
+            venta1.impresora = "Lexmar";
+            venta1.total = 10500;
+            Venta venta2 = new Venta();
+            venta2.impresora = "Hp Deskjet";
+            venta2.total = 10340;
 
             Cliente1 = new Cliente5Por();
             Cliente2 = new ClienteFijo();
             Cliente3 = new ClienteSinDescuento();
             Cliente4 = new ClienteSinDescuento();
-            Cliente1.impresora("Lexmar", 300);
-            Cliente2.impresora("Hp", 500);
-            Cliente1.aplicarDescuento();
-            Cliente2.aplicarDescuento();
+            Cliente1.aplicarDescuento(venta1, 16);
+            Cliente2.aplicarDescuento(venta2,16);
+            Console.ReadKey();
         }
     }
 }
